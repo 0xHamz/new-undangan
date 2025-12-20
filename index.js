b402fcdbdd70e61c7e1c518ab68def160589593e
0xHamz <60563853+0xHamz@users.noreply.github.com>
2025-12-20 14:58:59 +0700

    Update index.js

--- index.js
+++ index.js
@@ -66,10 +66,10 @@ app.delete("/api/guest/:id", async (req, res) => {
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, "frontend/dist")));
+app.use(express.static(path.join(__dirname, "dist")));
 
 app.use((req, res) => {
-  res.sendFile(path.join(__dirname, "frontend/dist/index.html"));
+  res.sendFile(path.join(__dirname, "dist/index.html"));
 });
 
 
