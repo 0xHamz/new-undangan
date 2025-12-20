ce4a01ee8a7753a691b80e96a890304ee1213472
0xHamz <60563853+0xHamz@users.noreply.github.com>
2025-12-20 14:55:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -68,9 +68,10 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, "frontend/dist")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend/dist/index.html"));
 });
+
 
 // ==========================
 const PORT = process.env.PORT || 5000;
